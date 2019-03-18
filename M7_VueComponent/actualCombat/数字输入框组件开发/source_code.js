420bd86108866107b04c4e78c1c5e629d9b8c147
ThorinGondor <1360002051@qq.com>
2019-03-18 10:06:50 +0800

    实战：基本功能开发

--- M7_VueComponent/actualCombat/数字输入框组件开发/source_code.js
+++ M7_VueComponent/actualCombat/数字输入框组件开发/source_code.js
@@ -1,17 +1,21 @@
 Vue.component('number-input', {
-    template: '<div class="num-ipt"></div>',
+    template: '<div class="num-ipt">\
+                   <input type="text" :value="currentValue" @change="handleChange">\
+                   <button @click="handleDecrease">-</button>\
+                   <button @click="handleIncrease">+</button>\
+               </div>',
     props: { //使用props来声明需要从父级接收的数据
         max: {
-            type: number,
+            type: Number,
             default: Infinity
         },
         min: {
-            type: number,
+            type: Number,
             default: -Infinity
         },
-        value: {
-            type: number,
-            default: 0
+        parent_value: {
+            type: Number,
+            default: 7
         }
     },
     data: function () {
@@ -21,8 +25,8 @@ Vue.component('number-input', {
     },
     watch: {
       currentValue: function (val) {
-          this.$emit('input', val);
-          this.$emit('on-change', val);
+          this.$emit('input', val); //传递给 :value
+          this.$emit('on-change', val);// 传递给 @change
       },
       parent_value: function (val) {
           this.updateValue(val);
@@ -33,6 +37,17 @@ Vue.component('number-input', {
             if (val>this.max) val = this.max;
             if (val<this.min) val = this.min;
             this.currentValue = val;
+        },
+        handleDecrease: function () {
+            if (this.currentValue<=this.min) return;
+            this.currentValue -= 1;
+        },
+        handleIncrease: function () {
+            if (this.currentValue>=this.max) return;
+            this.currentValue += 1;
+        },
+        handleChange: function () {
+
         }
     },
     mounted: function () {
